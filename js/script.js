6d7949e16712cbe098b86e571cd79d80e6712ccc
Hudson8811 <hudson88@yandex.ru>
2023-04-20 13:19:13 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -27,18 +27,44 @@ $(document).on('click', '.js-spoiler-title', function () {
 			$(this).parents('.js-spoiler').find('.js-spoiler-body').removeClass('active').removeClass('active');
 			$(this).parents('.js-spoiler-item').addClass('active');
 			$(this).parents('.js-spoiler-item').find('.js-spoiler-body').addClass('active');
+			if ($(window).width() < 991) {
+				$('body').addClass('lock')
+			} else {
+				$('body').removeClass('lock')
+			}
+
 		} else {
 			$(this).parents('.js-spoiler').find('.js-spoiler-item').removeClass('active');
 			$(this).parents('.js-spoiler').find('.js-spoiler-body').removeClass('active');
+			$('body').addClass('lock')
 		}
 	} else {
 		if (!$(this).parents('.js-spoiler-item').hasClass('active')) {
 			$(this).parents('.js-spoiler-item').addClass('active');
 			$(this).parents('.js-spoiler-item').find('.js-spoiler-body').addClass('active');
+			if ($(window).width() < 991) {
+				$('body').addClass('lock')
+			} else {
+				$('body').removeClass('lock')
+			}
 		} else {
 			$(this).parents('.js-spoiler-item').removeClass('active');
 			$(this).parents('.js-spoiler-item').find('.js-spoiler-body').removeClass('active');
+			$('body').addClass('lock')
 		}
+	}
+});
+
+$(document).on('click', '.js-spoiler-close', function () {
+	$(this).parents('.js-spoiler').find('.js-spoiler-item').removeClass('active');
+	$(this).parents('.js-spoiler').find('.js-spoiler-body').removeClass('active');
+	$('body').removeClass('lock')
+});
+$(document).on('click', '.js-spoiler-body', function (e) {
+	if (!$(e.target).closest('.js-spoiler-box').length && !$(e.target).closest('.js-spoiler-title').length) {
+		$('.js-spoiler').find('.js-spoiler-item').removeClass('active');
+		$('.js-spoiler').find('.js-spoiler-body').removeClass('active');
+		$('body').removeClass('lock');
 	}
 });
 // spoiler end ===============================================
